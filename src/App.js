2e7165517efeefcf110678b6671f7f2f378ca487
Hossein <stangn99@gmail.com>
2019-01-20 16:02:03 -0500

    Add the footer and some more styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component, Fragment } from 'react';
-
 import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Header />
+        <Footer />
+        
       </Fragment>
     );
   }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div>
+    <div className="headerWrapper">
      <h1>Header!</h1>
     </div>
   );
