1a010ec81d964e027bdda0d80c93eaa1aa9b6bc5
James Shin <jhscode@gmail.com>
2019-01-20 14:52:26 -0500

    added router, changed app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        Wrkflo
-        </header>
-      </div>
+      <Fragment>
+        Header!
+      </Fragment>
     );
   }
 }
