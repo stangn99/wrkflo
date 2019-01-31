b0924fe465aada78823d4c790427ad8c9fe927da
Hossein <stangn99@gmail.com>
2019-01-30 22:02:15 -0500

    Added handler for opening a Dialog

--- src/Components/ListItems/ListItems.js
+++ src/Components/ListItems/ListItems.js
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
+import Dialog from '@material-ui/core/Dialog';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/Inbox';
 import DraftsIcon from '@material-ui/icons/Drafts';
+import Slide from '@material-ui/core/Slide';
 
 
 const styles = theme => ({
@@ -21,27 +23,55 @@ function ListItemLink(props) {
   return <ListItem button component="a" {...props} />;
 }
 
-function SimpleList(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <List component="nav">
-        <ListItem button>
-          <ListItemIcon>
-            <InboxIcon />
-          </ListItemIcon>
-          <ListItemText primary="Create Task" />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <DraftsIcon />
-          </ListItemIcon>
-          <ListItemText primary="Create Project" />
-        </ListItem>
-      </List>
-    </div>
-  );
+function Transition(props) {
+    return <Slide direction="up" {...props} />;
 }
+
+class SimpleList extends React.Component {
+
+  state = {
+    open: false
+  }
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+ 
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <List component="nav">
+          <ListItem button>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Create Task" onClick={this.handleClickOpen} />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <DraftsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Create Project" />
+          </ListItem>
+        </List>
+        <Dialog
+          fullScreen
+          open={this.state.open}
+          onClose={this.handleClose}
+          TransitionComponent={Transition}
+        ></Dialog>
+      </div>
+      
+    )
+  }
+}
+
 
 SimpleList.propTypes = {
   classes: PropTypes.object.isRequired,
